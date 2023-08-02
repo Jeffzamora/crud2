87d5da193a0c544a2992606517ea3385428a6ba5
Jefferson Zamora <92548079+Jeffzamora@users.noreply.github.com>
2023-08-02 12:19:20 -0600

    Limpiand template

---
+++ view/header/header.php
@@ -0,0 +1,35 @@
+    <!-- ########## START: HEAD PANEL ########## -->
+    <div class="br-header">
+      <div class="br-header-left">
+        <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
+        <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
+      </div><!-- br-header-left -->
+      <div class="br-header-right">
+        <nav class="nav">
+             <div class="dropdown">
+              
+            <div class="dropdown-menu dropdown-menu-header wd-300 pd-0-force">
+              <div class="d-flex align-items-center justify-content-between pd-y-10 pd-x-20 bd-b bd-gray-200">
+             
+                 <div class="pd-y-10 tx-center bd-t">
+                  <a href="" class="tx-12"><i class="fa fa-angle-down mg-r-5"></i></a>
+                </div>
+              </div><!-- media-list -->
+            </div><!-- dropdown-menu -->
+             </div><!-- dropdown -->
+                  
+          <div class="dropdown">
+            <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+              <span class="logged-name hidden-md-down">Mario Espinoza</span>
+              <img src="http://via.placeholder.com/64x64" class="wd-32 rounded-circle" alt="">
+              <span class="square-10 bg-success"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-header wd-200">
+      
+                       </div><!-- dropdown-menu -->
+          </div><!-- dropdown -->
+                <div class="navicon-right">
+                 </div><!-- navicon-right -->
+      </div><!-- br-header-right -->
+    </div><!-- br-header -->
+    <!-- ########## END: HEAD PANEL ########## -->
